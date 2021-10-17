254a5c128723e3153dc799dce15f78a637cf6dfa
Sergey Kharitonov <hariton27sy@gmail.com>
2021-10-17 22:33:04 +0500

    Add database connection support

---
+++ src/main/java/com/example/moneydiary/model/User.java
@@ -0,0 +1,45 @@
+package com.example.moneydiary.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String username;
+    private String password;
+    private String email;
+
+    protected User(){}
+
+    public User(String username, String password, String email){
+
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User[id=%d, username=%s, email=%s]", userId, username, email);
+    }
+}
